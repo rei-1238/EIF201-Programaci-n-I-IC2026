f16cf0c0de229f2e6c97ad63d137ca46ce29efc0
Christopher Chavarria <christopher.chavarria.solorzano@est.una.ac.cr>
2026-03-13 17:51:28 -0600

    Hice un for nada mas para probar

--- ProyectoProgra/proyecto-Progra1/proyecto-Progra1.cpp
+++ ProyectoProgra/proyecto-Progra1/proyecto-Progra1.cpp
@@ -7,5 +7,12 @@ int main()
     
     std::cout <<"Chavarria probando codigo";
     std::cout << "Limpie cache del github, ya que tenia archivos que eran basura";
+
+    int array[] = { 1,2, 3 ,4 };
+    std::cout << "Ingrese un numero de elementos: ";
+    for(int i=0; i < 4; i++){
+        std::cout << "elementos" << array[i];
+    }
+    return 0;
 }
 
