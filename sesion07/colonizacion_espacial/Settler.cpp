06401eaf2e09978ebb9d5fd0b47f8843d86a6255
rachel aguilar carballo <rachel.aguilar.carballo@est.una.ac.cr>
2026-04-14 17:36:52 -0600

    Agregue el .cpp de Settler

--- sesion07/colonizacion_espacial/Settler.cpp
+++ sesion07/colonizacion_espacial/Settler.cpp
@@ -1,1 +1,40 @@
 #include "Settler.h"
+#include <iostream>
+
+namespace spacecolony {
+
+    Settler::Settler(int id, const std::string& name, const std::string& specialty,
+        double oxygenConsumption, int productivity)
+        : id(id), name(name), specialty(specialty),
+        oxygenConsumption(oxygenConsumption), productivity(productivity) {
+    }
+
+    int Settler::getId() const {
+        return id;
+    }
+
+    std::string Settler::getName() const {
+        return name;
+    }
+
+    std::string Settler::getSpecialty() const {
+        return specialty;
+    }
+
+    double Settler::getOxygenConsumption() const {
+        return oxygenConsumption;
+    }
+
+    int Settler::getProductivity() const {
+        return productivity;
+    }
+
+    void Settler::display() const {
+        std::cout << "ID: " << id
+            << " | Nombre: " << name
+            << " | Especialidad: " << specialty
+            << " | O2: " << oxygenConsumption
+            << " | Productividad: " << productivity << '\n';
+    }
+
+}
